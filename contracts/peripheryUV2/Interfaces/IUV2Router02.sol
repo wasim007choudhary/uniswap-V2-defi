2668888991729f819560cb65c6a20ff87c01a917
wasim007choudhary <lesnarwasimff5@gmail.com>
2026-07-02 01:00:39 +0530

    IUV2Router02 updated

--- contracts/peripheryUV2/Interfaces/IUV2Router02.sol
+++ contracts/peripheryUV2/Interfaces/IUV2Router02.sol
@@ -3,4 +3,12 @@ pragma solidity ^0.8.20;
 
 import {IUV2Router01} from "contracts/peripheryUV2/Interfaces/IUV2Router01.sol";
 
-interface IUV2Router02 is IUV2Router01 {}
+interface IUV2Router02 is IUV2Router01 {
+    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        uint256 inputAmount,
+        uint256 minOutputAmount,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external;
+}
